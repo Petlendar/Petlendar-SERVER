4adf4bf6dc6b49b5ef3a2c227a0502c4ff16a157
SEOB <svj7531@naver.com>
2024-09-22 02:13:41 +0900

    SB-26 (feat) : AuthorizationService 구현
    SB-26 (feat) : AuthorizationService 구현

--- db/src/main/java/db/user/UserRepository.java
+++ db/src/main/java/db/user/UserRepository.java
@@ -1,7 +1,11 @@
 package db.user;
 
+import db.user.enums.UserStatus;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findFirstByIdAndStatusOrderByIdDesc(Long userId, UserStatus userStatus);
 
 }
