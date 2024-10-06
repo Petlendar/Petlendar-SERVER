2f0998d298bc12cfecf0368c92b03951de3c0162
SEOB <svj7531@naver.com>
2024-10-06 18:23:01 +0900

    SB-131 (feat) :  Ai Module 초기 설정
    SB-131 (feat) :  Ai Module 초기 설정

---
+++ ai/src/main/java/ai/common/exception/UserExceptionHandler.java
@@ -0,0 +1,47 @@
+package ai.common.exception;
+
+import global.api.Api;
+import global.errorcode.UserErrorCode;
+import ai.common.exception.user.ExistUserEmailException;
+import ai.common.exception.user.ExistUserNameException;
+import ai.common.exception.user.LoginFailException;
+import ai.common.exception.user.UserNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class UserExceptionHandler {
+
+    @ExceptionHandler(value = UserNotFoundException.class)
+    public ResponseEntity<Api<Object>> userNotFoundException(UserNotFoundException e) {
+        log.info("", e);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(Api.ERROR(UserErrorCode.USER_NOT_FOUND));
+    }
+
+    @ExceptionHandler(value = ExistUserNameException.class)
+    public ResponseEntity<Api<Object>> existUserNameException(ExistUserNameException e) {
+        log.info("", e);
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+            .body(Api.ERROR(UserErrorCode.EXISTS_USER_NAME));
+    }
+
+    @ExceptionHandler(value = ExistUserEmailException.class)
+    public ResponseEntity<Api<Object>> existUserEmailException(ExistUserEmailException e) {
+        log.info("", e);
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+            .body(Api.ERROR(UserErrorCode.EXISTS_USER_EMAIL));
+    }
+
+    @ExceptionHandler(value = LoginFailException.class)
+    public ResponseEntity<Api<Object>> loginFailException(LoginFailException e) {
+        log.info("", e);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+            .body(Api.ERROR(UserErrorCode.LOGIN_FAIL));
+    }
+
+}
