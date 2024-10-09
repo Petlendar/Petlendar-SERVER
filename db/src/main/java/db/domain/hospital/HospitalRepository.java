7bc719e3cd9f590c6fd87f4115a3971159912b8b
SEOB <svj7531@naver.com>
2024-10-09 14:03:52 +0900

    SB-137 (feat) : Hospital Repository 개발
    SB-137 (feat) : Hospital Repository 개발

---
+++ db/src/main/java/db/domain/hospital/HospitalRepository.java
@@ -0,0 +1,7 @@
+package db.domain.hospital;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HospitalRepository extends JpaRepository<HospitalEntity, Long> {
+
+}
